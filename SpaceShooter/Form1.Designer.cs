829eb499c8f634e4e5dda662fdb6fb2460a75714
Ingrid <oltingrid@gmail.com>
2025-05-05 00:42:53 +0300

    SpaceShooter project

---
+++ SpaceShooter/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace SpaceShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MoveBgTimer = new System.Windows.Forms.Timer(this.components);
+            this.Aircraft = new System.Windows.Forms.PictureBox();
+            this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveMunitionTimer = new System.Windows.Forms.Timer(this.components);
+            this.EnemisMunitionTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveEnemisTimer = new System.Windows.Forms.Timer(this.components);
+            this.ReplayBtn = new System.Windows.Forms.Button();
+            this.QuitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.levelbl = new System.Windows.Forms.Label();
+            this.scorelbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Aircraft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoveBgTimer
+            // 
+            this.MoveBgTimer.Enabled = true;
+            this.MoveBgTimer.Interval = 20;
+            this.MoveBgTimer.Tick += new System.EventHandler(this.MoveBgTimer_Tick);
+            // 
+            // Aircraft
+            // 
+            this.Aircraft.BackColor = System.Drawing.Color.Transparent;
+            this.Aircraft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Aircraft.BackgroundImage")));
+            this.Aircraft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Aircraft.Location = new System.Drawing.Point(270, 390);
+            this.Aircraft.Name = "Aircraft";
+            this.Aircraft.Size = new System.Drawing.Size(50, 50);
+            this.Aircraft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Aircraft.TabIndex = 0;
+            this.Aircraft.TabStop = false;
+            // 
+            // LeftMoveTimer
+            // 
+            this.LeftMoveTimer.Interval = 5;
+            this.LeftMoveTimer.Tick += new System.EventHandler(this.LeftMoveTimer_Tick);
+            // 
+            // RightMoveTimer
+            // 
+            this.RightMoveTimer.Interval = 5;
+            this.RightMoveTimer.Tick += new System.EventHandler(this.RightMoveTimer_Tick);
+            // 
+            // DownMoveTimer
+            // 
+            this.DownMoveTimer.Interval = 5;
+            this.DownMoveTimer.Tick += new System.EventHandler(this.DownMoveTimer_Tick);
+            // 
+            // UpMoveTimer
+            // 
+            this.UpMoveTimer.Interval = 5;
+            this.UpMoveTimer.Tick += new System.EventHandler(this.UpMoveTimer_Tick);
+            // 
+            // MoveMunitionTimer
+            // 
+            this.MoveMunitionTimer.Enabled = true;
+            this.MoveMunitionTimer.Interval = 20;
+            this.MoveMunitionTimer.Tick += new System.EventHandler(this.MoveMunitionTimer_Tick);
+            // 
+            // EnemisMunitionTimer
+            // 
+            this.EnemisMunitionTimer.Enabled = true;
+            this.EnemisMunitionTimer.Interval = 20;
+            this.EnemisMunitionTimer.Tick += new System.EventHandler(this.EnemisMunitionTimer_Tick);
+            // 
+            // MoveEnemisTimer
+            // 
+            this.MoveEnemisTimer.Enabled = true;
+            this.MoveEnemisTimer.Tick += new System.EventHandler(this.MoveEnemisTimer_Tick);
+            // 
+            // ReplayBtn
+            // 
+            this.ReplayBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReplayBtn.Location = new System.Drawing.Point(175, 215);
+            this.ReplayBtn.Name = "ReplayBtn";
+            this.ReplayBtn.Size = new System.Drawing.Size(225, 52);
+            this.ReplayBtn.TabIndex = 1;
+            this.ReplayBtn.Text = "Replay";
+            this.ReplayBtn.UseVisualStyleBackColor = true;
+            this.ReplayBtn.Visible = false;
+            this.ReplayBtn.Click += new System.EventHandler(this.ReplayBtn_Click);
+            // 
+            // QuitBtn
+            // 
+            this.QuitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuitBtn.Location = new System.Drawing.Point(175, 285);
+            this.QuitBtn.Name = "QuitBtn";
+            this.QuitBtn.Size = new System.Drawing.Size(225, 56);
+            this.QuitBtn.TabIndex = 2;
+            this.QuitBtn.Text = "Quit";
+            this.QuitBtn.UseVisualStyleBackColor = true;
+            this.QuitBtn.Visible = false;
+            this.QuitBtn.Click += new System.EventHandler(this.QuitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Castellar", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(197, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 58);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LABEL";
+            this.label1.Visible = false;
+            // 
+            // levelbl
+            // 
+            this.levelbl.AutoSize = true;
+            this.levelbl.BackColor = System.Drawing.Color.Transparent;
+            this.levelbl.Font = new System.Drawing.Font("Rockwell", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.levelbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.levelbl.Location = new System.Drawing.Point(461, 10);
+            this.levelbl.Name = "levelbl";
+            this.levelbl.Size = new System.Drawing.Size(39, 30);
+            this.levelbl.TabIndex = 4;
+            this.levelbl.Text = "01";
+            // 
+            // scorelbl
+            // 
+            this.scorelbl.AutoSize = true;
+            this.scorelbl.BackColor = System.Drawing.Color.Transparent;
+            this.scorelbl.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.scorelbl.Location = new System.Drawing.Point(105, 9);
+            this.scorelbl.Name = "scorelbl";
+            this.scorelbl.Size = new System.Drawing.Size(108, 31);
+            this.scorelbl.TabIndex = 5;
+            this.scorelbl.Text = "scorelbl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(351, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Level:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scorelbl);
+            this.Controls.Add(this.levelbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.QuitBtn);
+            this.Controls.Add(this.ReplayBtn);
+            this.Controls.Add(this.Aircraft);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 700);
+            this.Name = "Form1";
+            this.Text = "SpaceShooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Aircraft)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MoveBgTimer;
+        private System.Windows.Forms.PictureBox Aircraft;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.Timer DownMoveTimer;
+        private System.Windows.Forms.Timer UpMoveTimer;
+        private System.Windows.Forms.Timer MoveMunitionTimer;
+        private System.Windows.Forms.Timer EnemisMunitionTimer;
+        private System.Windows.Forms.Timer MoveEnemisTimer;
+        private System.Windows.Forms.Button ReplayBtn;
+        private System.Windows.Forms.Button QuitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label levelbl;
+        private System.Windows.Forms.Label scorelbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SpaceShooter/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceShooter
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] Stars;
+        PictureBox[] Enemis;
+        PictureBox[] Munitions;
+        PictureBox[] EnemisMunition;
+
+        Random rnd;
+
+
+        int BackgroundSpeed;
+        int MunitionSpeed;
+        int AircraftSpeed;
+        int EnemiSpeed;
+        int EnemisMunitionSpeed;
+        int Score;
+        int Level;
+        int hard;
+        bool pause;
+        bool gameIsOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pause = false;
+            gameIsOver = false;
+            BackgroundSpeed = 4;
+            EnemisMunitionSpeed = 4;
+            MunitionSpeed = 20;
+            AircraftSpeed = 6;
+            EnemiSpeed = 4;
+            Score = 0;
+            Level = 1;
+            hard = 9;
+
+            Image enemi1 = Image.FromFile("asserts\\E1.png");
+            Image enemi2 = Image.FromFile("asserts\\E2.png");
+            Image enemi3 = Image.FromFile("asserts\\E3.png");
+            Image boss1 = Image.FromFile("asserts\\Boss1.png");
+            Image boss2 = Image.FromFile("asserts\\Boss2.png");
+            Image munition = Image.FromFile("asserts\\munition.png");
+
+            Stars = new PictureBox[10];
+            Enemis = new PictureBox[10];
+            Munitions = new PictureBox[3];
+            EnemisMunition = new PictureBox[10];
+
+
+            rnd = new Random();
+
+            for (int i = 0; i < Stars.Length; i++)
+            {
+                Stars[i] = new PictureBox();
+                Stars[i].BorderStyle = BorderStyle.None;
+                Stars[i].Location = new Point(rnd.Next(20, 580), rnd.Next(-10, 400));
+                if (i % 2 == 1)
+                {
+                    Stars[i].Size = new Size(2, 2);
+                    Stars[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    Stars[i].Size = new Size(3, 3);
+                    Stars[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(Stars[i]);
+            }
+
+            for (int i = 0; i < Enemis.Length; i++)
+            {
+                Enemis[i] = new PictureBox();
+                Enemis[i].Size = new Size(40, 40);
+                Enemis[i].SizeMode = PictureBoxSizeMode.Zoom;
+                Enemis[i].BorderStyle = BorderStyle.None;
+                Enemis[i].Visible = false;
+                this.Controls.Add(Enemis[i]);
+                Enemis[i].Location = new Point((i + 1) * 50, -50);
+            }
+
+            Enemis[0].Image = boss1;
+            Enemis[1].Image = enemi2;
+            Enemis[2].Image = enemi3;
+            Enemis[3].Image = enemi3;
+            Enemis[4].Image = enemi1;
+            Enemis[5].Image = enemi3;
+            Enemis[6].Image = enemi2;
+            Enemis[7].Image = enemi3;
+            Enemis[8].Image = enemi2;
+            Enemis[9].Image = boss2;
+
+            //Enemis Munition          
+            for (int i = 0; i < EnemisMunition.Length; i++)
+            {
+                EnemisMunition[i] = new PictureBox();
+                EnemisMunition[i].Size = new Size(2, 25);
+                EnemisMunition[i].Visible = false;
+                EnemisMunition[i].BackColor = Color.Yellow;
+                int x = rnd.Next(0, 10);
+                EnemisMunition[i].Location = new Point(Enemis[x].Location.X, Enemis[x].Location.Y - 20);
+                this.Controls.Add(EnemisMunition[i]);
+            }
+
+            for (int i = 0; i < Munitions.Length; i++)
+            {
+                Munitions[i] = new PictureBox();
+                Munitions[i].Size = new Size(8, 8);
+                Munitions[i].Image = munition;
+                Munitions[i].SizeMode = PictureBoxSizeMode.Zoom;
+                Munitions[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(Munitions[i]);
+            }
+        }
+
+        private void MoveBgTimer_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < Stars.Length / 2; i++)
+            {
+                Stars[i].Top += BackgroundSpeed;
+                if (Stars[i].Top >= this.Height)
+                {
+                    Stars[i].Top = -Stars[i].Height;
+                }
+            }
+
+            for (int i = Stars.Length / 2; i < Stars.Length; i++)
+            {
+                Stars[i].Top += BackgroundSpeed - 2;
+                if (Stars[i].Top >= this.Height)
+                {
+                    Stars[i].Top = -Stars[i].Height;
+                }
+            }
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Aircraft.Left > 10)
+            {
+                Aircraft.Left -= AircraftSpeed;
+            }
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Aircraft.Right < 420)
+            {
+                Aircraft.Left += AircraftSpeed;
+            }
+        }
+
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Aircraft.Top < 320)
+            {
+                Aircraft.Top += AircraftSpeed;
+            }
+        }
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Aircraft.Top > 10)
+            {
+                Aircraft.Top -= AircraftSpeed;
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!pause)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    RightMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    LeftMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    DownMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    UpMoveTimer.Start();
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            RightMoveTimer.Stop();
+            LeftMoveTimer.Stop();
+            DownMoveTimer.Stop();
+            UpMoveTimer.Stop();
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!gameIsOver)
+                {
+                    if (pause)
+                    {
+                        StartTimers();
+                        label1.Visible = false;
+                        pause = false;
+                    }
+                    else
+                    {
+                        label1.Location = new Point(20, 150);
+                        label1.Text = "GAME PAUSED";
+                        label1.Visible = true;
+                        StopTimers();
+                        pause = true;
+                    }
+                }
+            }
+        }
+        private void MoveMunitionTimer_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < Munitions.Length; i++)
+            {
+                if (Munitions[i].Top > 0)
+                {
+                    Munitions[i].Visible = true;
+                    Munitions[i].Top -= MunitionSpeed;
+
+                    Collision();
+                }
+                else
+                {
+                    Munitions[i].Visible = false;
+                    Munitions[i].Location = new Point(Aircraft.Location.X + 20, Aircraft.Location.Y - i * 30);
+                }
+            }
+        }
+
+        private void EnemisMunitionTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < (EnemisMunition.Length - hard); i++)
+            {
+                if (EnemisMunition[i].Top < this.Height)
+                {
+                    EnemisMunition[i].Visible = true;
+                    EnemisMunition[i].Top += EnemisMunitionSpeed;
+                    CollisionWithEnemisMunition();
+                }
+                else
+                {
+                    EnemisMunition[i].Visible = false;
+                    int x = rnd.Next(0, 10);
+                    EnemisMunition[i].Location = new Point(Enemis[x].Location.X + 20, Enemis[x].Location.Y + 30);
+                }
+            }
+        }
+
+        //Move Enemies Timer_Tick
+        private void MoveEnemisTimer_Tick(object sender, EventArgs e)
+        {
+            MoveEnemisVertical(Enemis, EnemiSpeed);
+        }
+
+        //Enemi Vertical Move Method
+        private void MoveEnemisVertical(PictureBox[] enemis, int speed)
+        {
+            for (int i = 0; i < enemis.Length; i++)
+            {
+                enemis[i].Visible = true;
+                enemis[i].Top += speed;
+
+                if (enemis[i].Top > this.Height)
+                {
+                    Enemis[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+        private void CollisionWithEnemisMunition()
+        {
+            for (int i = 0; i < EnemisMunition.Length; i++)
+            {
+                if (EnemisMunition[i].Bounds.IntersectsWith(Aircraft.Bounds))
+                {
+                    EnemisMunition[i].Visible = false;
+                    Aircraft.Visible = false;
+                    GameOver("Game Over");
+                }
+            }
+        }
+        private void Collision()
+        {
+            for (int i = 0; i < Enemis.Length; i++)
+            {
+                if (Munitions[0].Bounds.IntersectsWith(Enemis[i].Bounds) || 
+                    Munitions[1].Bounds.IntersectsWith(Enemis[i].Bounds) || 
+                    Munitions[2].Bounds.IntersectsWith(Enemis[i].Bounds))
+                {
+                    Score += 1;
+                    scorelbl.Text = (Score < 10) ? "0" + Score.ToString() : Score.ToString();
+
+                    if (Score % 30 == 0)
+                    {
+                        Level += 1;
+                        levelbl.Text = (Level < 10) ? "0" + Level.ToString() : Level.ToString();
+
+                        if (EnemiSpeed <= 10 && EnemisMunitionSpeed <= 10 && hard >= 0)
+                        {
+                            hard--;
+                            EnemiSpeed++;
+                            EnemisMunitionSpeed++;
+                        }
+
+                        if (Level == 10)
+                        {
+                            GameOver("NICE DONE");
+                        }
+                    }
+                    Enemis[i].Location = new Point((i + 1) * 50, -100);
+                }
+                if (Aircraft.Bounds.IntersectsWith(Enemis[i].Bounds))
+                {
+                    Aircraft.Visible = false;
+                    GameOver("GAME OVER");
+                }
+            }
+        }
+        private void GameOver(String str)
+        {
+            label1.Location = new Point(60, 110);
+            label1.Text = str.Trim();
+            label1.Visible = true;
+            gameIsOver = true;
+            pause = true;
+            ReplayBtn.Text = "Replay";
+            ReplayBtn.Visible = true;
+            QuitBtn.Visible = true;
+            StopTimers();
+        }
+
+       
+        private void ReplayBtn_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+        }
+
+         
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+       
+        private void StopTimers()
+        {
+            MoveBgTimer.Stop();
+            MoveEnemisTimer.Stop();
+            MoveMunitionTimer.Stop();
+            EnemisMunitionTimer.Stop();
+        }
+
+        private void StartTimers()
+        {
+            MoveBgTimer.Start();
+            MoveEnemisTimer.Start();
+            MoveMunitionTimer.Start();
+            EnemisMunitionTimer.Start();
+        }
+    }
+
+}
+
